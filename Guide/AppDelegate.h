d8716ed240236271071fda454f4310713b2981c2
ourLeader <973490661@qq.com>
2016-04-07 11:35:38 +0800

    Initial Commit

---
+++ Guide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Guide
+//
+//  Created by ksm on 16/4/7.
+//  Copyright © 2016年 ksm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
